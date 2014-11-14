268ce0bfe84ea207cf72fafafe54ee06ade22a91
Allan Baptista <allan.marques@ymail.com>
2014-11-14 18:26:45 -0200

    Adicionado a classe de input.

--- scripts/game/Input.js
+++ scripts/game/Input.js
@@ -1,5 +1,38 @@
+ const SPACE = 32,
+       LEFT = 37,
+       RIGHT = 39;
+
 export default class Input {
     constructor() {
-        
+        this.currentKeys = {};
+
+        window.addEventListener('keyup', this.onKeyUp.bind(this));
+        window.addEventListener('keydown', this.onKeyDown.bind(this));
+    }
+
+    onKeyUp(e) {
+        this.currentKeys[e.keyCode] = false;
+    }
+
+    onKeyDown(e) {
+        this.currentKeys[e.keyCode] = true;
+    }
+
+    isLeftPressed() {
+        let currKeys = this.currentKeys;
+
+        return LEFT in currKeys && currKeys[LEFT] === true;
+    }
+
+    isRightPressed() {
+        var currKeys = this.currentKeys;
+
+        return RIGHT in currKeys && currKeys[RIGHT] === true;
+    }
+
+    isActionPressed() {
+        var currKeys = this.currentKeys;
+
+        return SPACE in currKeys && currKeys[SPACE] === true;
     }
 }
